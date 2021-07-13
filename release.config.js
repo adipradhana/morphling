8eeddeb6861188e9164e3eded67e75ab730cabfd
adeyahya <adeyahyaprasetyo@gmail.com>
2021-07-13 15:32:11 +0700

    feat: test semantic release

---
+++ release.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  release: {
+    branch: 'master',
+  },
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/gitlab',
+      {
+        gitlabUrl: 'https://gitlab.warungpintar.co',
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
